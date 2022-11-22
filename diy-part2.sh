fcf233c0f924d622a0cc139dbbdadcb180654576
xu26311365 <33450967+xu26311365@users.noreply.github.com>
2022-11-22 22:12:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,3 +23,25 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 #sed -i '/spi-max-frequency/a\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_d-team_newifi-d2.dts
 sed -i 's/reg = <0x50000 0x1fb0000>/reg = <0x50000 0x3fb0000>/g' target/linux/ramips/dts/mt7621_d-team_newifi-d2.dts
 sed -i 's/32448k/64521k/g' target/linux/ramips/image/mt7621.mk
+
+# 把bootstrap替换成argon为源码必选主题（可自行修改您要的,主题名称必须对,比如下面代码的[argon],源码内必须有该主题,要不然编译失败）
+sed -i "s/bootstrap/argon/ig" feeds/luci/collections/luci/Makefile
+
+# 设置首次登录后台密码为空（进入openwrt后自行修改密码）
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' "${ZZZ_PATH}"
+
+# 删除默认防火墙
+sed -i '/to-ports 53/d' "${ZZZ_PATH}"
+
+
+# 修改插件名字
+sed -i 's/"aMule设置"/"电驴下载"/g' `egrep "aMule设置" -rl ./`
+sed -i 's/"网络存储"/"NAS"/g' `egrep "网络存储" -rl ./`
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `egrep "Turbo ACC 网络加速" -rl ./`
+sed -i 's/"实时流量监测"/"流量"/g' `egrep "实时流量监测" -rl ./`
+sed -i 's/"KMS 服务器"/"KMS激活"/g' `egrep "KMS 服务器" -rl ./`
+sed -i 's/"TTYD 终端"/"命令窗"/g' `egrep "TTYD 终端" -rl ./`
+sed -i 's/"USB 打印服务器"/"打印服务"/g' `egrep "USB 打印服务器" -rl ./`
+sed -i 's/"Web 管理"/"Web管理"/g' `egrep "Web 管理" -rl ./`
+sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
+sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
